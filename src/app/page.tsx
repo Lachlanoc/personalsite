d0475e12e6ceb8d017d72bc9bebf509e0631b1e0
Lachlan <lachlanoco@gmail.com>
2025-07-04 17:58:56 +1000

    Fixed getSortedPostsData to now have a working posts page. Can move onto aesthetics.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
+import Header from '../components/Header';
+import React from 'react';
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center min-h-screen bg-white'>
+        <Header />
       <h1 className="text-4xl font-bold mb-4">Welcome to My Blog</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       <Link href="/posts" className="text-black-600 underline">View All Posts</Link>
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -5,8 +5,14 @@ import matter from 'gray-matter';
 const postsDirectory = path.join(process.cwd(), 'src', 'app', 'posts');
 
 export async function getSortedPostsData() {
-  const fileNames = await fs.readdir(postsDirectory);
-  const allPostsData = await Promise.all(fileNames.map(async fileName => {
+  const fileNames = await fs.readdir(postsDirectory, { withFileTypes: true });
+
+  // Only include files (not directories) that end with .md
+  const mdFileNames = fileNames
+    .filter(dirent => dirent.isFile() && dirent.name.endsWith('.md'))
+    .map(dirent => dirent.name);
+
+  const allPostsData = await Promise.all(mdFileNames.map(async fileName => {
     const slug = fileName.replace(/\.md$/, '');
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = await fs.readFile(fullPath, 'utf8');
@@ -16,6 +22,7 @@ export async function getSortedPostsData() {
       ...(matterResult.data as { title: string; date: string })
     };
   }));
+
   return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
 }
 
